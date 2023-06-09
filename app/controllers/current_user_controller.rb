d65a1785469deccc3275c72f88cd58dee0de106a
Hipjea <pierre.duverneix@gmail.com>
2023-06-09 11:55:26 +0200

    current_user

---
+++ app/controllers/current_user_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CurrentUserController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    render json: UserSerialzier.new(current_user).serializeable_hash[:data][:attributes], status: :ok
+  end
+end
