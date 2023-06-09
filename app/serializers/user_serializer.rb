40f15e9406d6b0d5f158092558898c913797081a
Hipjea <pierre.duverneix@gmail.com>
2023-06-09 11:33:52 +0200

    devise controllers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,10 @@
 
 class UserSerializer
   include JSONAPI::Serializer
+
   attributes :id, :email, :created_at
+
+  attribute :created_date do |user|
+    user.created_at && user.created_at.strftime('%d/%m/%Y')
+  end  
 end
