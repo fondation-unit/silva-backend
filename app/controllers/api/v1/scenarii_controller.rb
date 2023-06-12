73eae17727d45dc30360bb98b756fce89e081cb9
CarolineMVB <caroline.beauchet@yahoo.fr>
2023-06-12 16:14:11 +0200

    add scenarii

--- app/controllers/api/v1/scenarii_controller.rb
+++ app/controllers/api/v1/scenarii_controller.rb
@@ -1,3 +1,13 @@
 class Api::V1::ScenariiController < ApplicationController
 
+    def index
+        @scenarii = Scenario.all
+        render json: ScenarioSerializer.new(@scenarii).serializable_hash[:data], status: :ok
+    end
+
+    def show
+        @scenario = Scenario.find(params[:id])
+        render json: ScenarioSerializer.new(@scenario).serializable_hash[:data][:attributes], status: :ok
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_092359) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_12_100315) do
+  create_table "scenarii", charset: "utf8mb4", force: :cascade do |t|
+    t.string "author"
+    t.string "name"
+    t.integer "level"
+    t.string "subject"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
