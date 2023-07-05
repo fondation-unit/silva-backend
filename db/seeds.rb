33814a1b96902dca55f7dabb3d020f8d7411e0a5
Hipjea <pierre.duverneix@gmail.com>
2023-07-05 09:46:53 +0200

    seeds scenario

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+22.times { |index|
+  Scenario.create!(
+    author: "Author #{rand(1..5)}",
+    name: "Scenario #{index + 1}",
+    level: "Level #{rand(1..5)}",
+    subject: "#{(0...8).map { ([65, 97].sample + rand(26)).chr }.push(rand(99)).join}",
+    description: "#{(0...100).map { ([65, 97].sample + rand(26)).chr }.push(rand(99)).join}"
+  )
+}
