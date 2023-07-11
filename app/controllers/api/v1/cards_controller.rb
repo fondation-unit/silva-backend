86b1a3a14d970d6b436a25335aab2d8e59300b8d
CarolineMVB <caroline.beauchet@yahoo.fr>
2023-07-11 17:03:29 +0200

    edit card crud

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -10,4 +10,29 @@ class Api::V1::CardsController < ApplicationController
     @card = Card.find(params[:id])
     render json: CardSerializer.new(@card).serializable_hash[:data][:attributes], status: :ok
   end
+
+  def create
+    @card = Card.new(card_params)
+
+    return unless @card.save
+
+    render json: {
+      status: { code: 200, message: "Card created successfully." },
+      data: CardSerializer.new(@card)
+    }
+  end
+
+  def update
+    @card = Card.find(params[:id])
+    @card.update(card_params)
+
+    render json: CardSerializer.new(@card).serializable_hash[:data][:attributes], status: :ok
+  end
+
+  private
+
+    def card_params
+      params.require(:card).permit(:typeable_type, attributes: %i[name description note typeable_type typeable_id])
+    end
 end
+
